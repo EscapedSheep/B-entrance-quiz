dfc3f09af18af07583ace5cd8cad055c4998f921
EscapedSheep <sfgmdf001@126.com>
2020-09-07 21:47:18 +0800

    [maida] test: add api test for change team name

--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentApiTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentApiTest.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.capability.gtb.entrancequiz.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.capability.gtb.entrancequiz.domain.ChangeTeamNameRequest;
 import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +42,19 @@ class StudentApiTest {
     @Test
     void should_add_student_to_list_given_student_name() throws Exception {
         mockMvc.perform(post("/students").content("newName").contentType(MediaType.TEXT_PLAIN))
+                .andExpect(status().isCreated())
                 .andExpect(jsonPath("$", hasSize(36)))
                 .andExpect(jsonPath("$[35].name", is("newName")));
+    }
+
+    @Test
+    void should_change_team_name_given_request() throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper();
+        ChangeTeamNameRequest request = new ChangeTeamNameRequest("Team 1", "new Team 1");
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/students/team").contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].teamName", is("new Team 1")));
     }
 }
