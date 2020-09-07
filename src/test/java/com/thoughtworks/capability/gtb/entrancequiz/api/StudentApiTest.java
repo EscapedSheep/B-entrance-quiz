1e0b792c1e20ec252318a8e8d5dfbead68313ce5
EscapedSheep <sfgmdf001@126.com>
2020-09-07 21:42:37 +0800

    [maida] test: add api test for add student

--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentApiTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentApiTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,5 +29,18 @@ class StudentApiTest {
     void should_return_students_list_when_call_api() throws Exception {
         mockMvc.perform(get("/students")).andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(35)));
+    }
+
+    @Test
+    void should_return_team_list_when_call_api() throws Exception {
+        mockMvc.perform(get("/students/team")).andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(6)));
+    }
+
+    @Test
+    void should_add_student_to_list_given_student_name() throws Exception {
+        mockMvc.perform(post("/students").content("newName").contentType(MediaType.TEXT_PLAIN))
+                .andExpect(jsonPath("$", hasSize(36)))
+                .andExpect(jsonPath("$[35].name", is("newName")));
     }
 }
